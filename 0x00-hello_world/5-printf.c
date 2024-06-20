1f032102cb2056d2f4edd21703bd77062776e94a
SaadElAmmari <giochi6@hotmail.com>
2024-06-20 05:21:28 -0700

    hey

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,19 @@
+/*
+ * File: 5-printf.c
+ * Description: A C program that prints a specific string with proper grammar.
+ */
+
 #include <stdio.h>
 
 /**
- * main - prints "with proper grammar, but the outcome is
- *  a piece of art," followed newline
- * Return: Always 0.
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+    /* Print the desired string */
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+
+    return (0);
 }
