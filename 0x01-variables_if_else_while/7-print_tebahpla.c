cfe1e83522c5e65781c9428c2daf1b3055fb5d19
SaadElAmmari <giochi6@hotmail.com>
2024-06-21 12:23:18 -0700

    hey

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,15 +2,15 @@
 
 int main(void)
 {
-	char letter = 'z'; // Start from 'z', the last character in the alphabet
+	char letter = 'z'; /* Start from 'z', the last character in the alphabet */
 	
 	while (letter >= 'a')
 	{
-		putchar(letter); // Print each letter using putchar
-		letter--; // Move to the previous character in the alphabet
+		putchar(letter); /* Print each letter using putchar */
+		letter--; /* Move to the previous character in the alphabet */
 	}
 	
-	putchar('\n'); // Print a newline at the end
+	putchar('\n'); /* Print a newline at the end */
 	
-	return (0);
+	return 0;
 }
