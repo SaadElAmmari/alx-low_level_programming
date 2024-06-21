6d2886fc4f6caf2b5570346f5b9a87aef3aa21ee
SaadElAmmari <giochi6@hotmail.com>
2024-06-21 12:40:12 -0700

    hey

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char digit;
+
+    // Print numbers '0' to '9'
+    for (digit = '0'; digit <= '9'; digit++)
+    {
+        putchar(digit);
+    }
+
+    // Print letters 'a' to 'f'
+    for (digit = 'a'; digit <= 'f'; digit++)
+    {
+        putchar(digit);
+    }
+
+    // Print newline character
+    putchar('\n');
+
+    return 0;
+}
