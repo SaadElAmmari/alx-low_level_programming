f57d622bee3313d7fd66daddce3de0f853363bc0
SaadElAmmari <giochi6@hotmail.com>
2024-06-21 12:26:52 -0700

    hey

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the lowercase alphabet in reverse order
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	char letter = 'z'; /* Start from 'z', the last character in the alphabet */
-	
+
 	while (letter >= 'a')
 	{
 		putchar(letter); /* Print each letter using putchar */
 		letter--; /* Move to the previous character in the alphabet */
 	}
-	
+
 	putchar('\n'); /* Print a newline at the end */
-	
-	return 0;
+
+	return (0);
 }
