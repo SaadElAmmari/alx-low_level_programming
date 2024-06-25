01c8482acc1d7f009ef285bc8a3d6a2ff7ffb0ab
SaadElAmmari <giochi6@hotmail.com>
2024-06-25 13:49:52 -0700

    hey

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,20 +14,22 @@ void times_table(void)
             product = row * column;
 
             if (column == 0)
-                _putchar('0');
-            else if (product < 10)
-                _putchar(' ');  /* extra space for single-digit numbers */
-            else
-                _putchar((product / 10) + '0');  /* tens digit */
-
-            _putchar((product % 10) + '0');  /* units digit */
-
-            if (column < 9)
             {
-                _putchar(',');  /* separate each number with a comma */
-                _putchar(' ');  /* add space after comma */
+                _putchar('0');
+            }
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
+
+                if (product < 10)
+                    _putchar(' ');
+                else
+                    _putchar((product / 10) + '0');
+
+                _putchar((product % 10) + '0');
             }
         }
-        _putchar('\n');  /* move to the next line after each row */
+        _putchar('\n');
     }
 }
