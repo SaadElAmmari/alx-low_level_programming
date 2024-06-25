c68d8bd741a986b8cb35631df39cb597d625e766
SaadElAmmari <giochi6@hotmail.com>
2024-06-25 14:22:34 -0700

    hey

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,4 @@
-#include <stdio.h>  /* Include the standard I/O header for printf */
+#include <stdio.h> /* Include the standard I/O header for printf */
 
 /**
  * print_to_98 - Prints all natural numbers from n to 98, followed by a new line
@@ -8,28 +8,28 @@
  */
 void print_to_98(int n)
 {
-    int i;  /* Declare loop control variable outside the for loop */
+	int i; /* Declare loop control variable outside the for loop */
 
-    /* Print numbers from n to 98 */
-    if (n <= 98)
-    {
-        /* Incrementing loop */
-        for (i = n; i <= 98; ++i)
-        {
-            printf("%d", i);
-            if (i != 98)
-                printf(", ");
-        }
-    }
-    else
-    {
-        /* Decrementing loop */
-        for (i = n; i >= 98; --i)
-        {
-            printf("%d", i);
-            if (i != 98)
-                printf(", ");
-        }
-    }
-    printf("\n");  /* Print newline at the end */
+	/* Print numbers from n to 98 */
+	if (n <= 98)
+	{
+		/* Incrementing loop */
+		for (i = n; i <= 98; ++i)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	else
+	{
+		/* Decrementing loop */
+		for (i = n; i >= 98; --i)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+	}
+	printf("\n"); /* Print newline at the end */
 }
