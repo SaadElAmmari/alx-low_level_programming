ed577db270134ae3724e36635e8581c03a6a3972
SaadElAmmari <giochi6@hotmail.com>
2024-06-25 12:30:31 -0700

    hey

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the alphabet 10 times, each followed by a new line
+ * print_alphabet_x10 - Prints the alphabet 10 times,
+ * each followed by a new line
  * Return: void
  */
 void print_alphabet_x10(void)
