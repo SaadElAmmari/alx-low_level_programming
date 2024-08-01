d855eb77bc002839abbbac272a9823ea77322850
SaadElAmmari <giochi6@hotmail.com>
2024-08-01 15:45:36 -0700

    hey

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * by SaadElAmmari
+ *  * @head: head of list
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+	free(head);
+}
