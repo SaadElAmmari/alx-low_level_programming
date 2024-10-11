8ddc51b65b9480653967fdac38a9c4db94eb885d
SaadElAmmari <giochi6@hotmail.com>
2024-10-11 13:57:23 -0700

    finish

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,28 +1,27 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: size of the array
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
  *
- * Return: pointer to the newly created hash table
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
-	unsigned long int n;
+	hash_table_t *ip;
+	unsigned long int mm;
 
-	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL)
+	ip = malloc(sizeof(hash_table_t));
+	if (ip == NULL)
 		return (NULL);
-	hash_table->size = size;
-	hash_table->array = malloc(size * sizeof(hash_node_t *));
-	if (hash_table->array == NULL)
-	{
-		free(hash_table);
+
+	ip->size = size;
+	ip->array = malloc(sizeof(hash_node_t *) * size);
+	if (ip->array == NULL)
 		return (NULL);
-	}
-	for (n = 0; n < size; n++)
-		hash_table->array[n] = NULL;
-	return (hash_table);
+	for (mm = 0; mm < size; mm++)
+		ip->array[mm] = NULL;
+
+	return (ip);
 }
-
